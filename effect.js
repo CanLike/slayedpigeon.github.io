7eb771ff8b2d8664a39ce81b13bfa099b42ca09f
CanLike <111686654+CanLike@users.noreply.github.com>
2022-12-15 18:38:40 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -9,10 +9,10 @@ $('document').ready(function(){
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
 			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
+			$('#b33').animate({top:240, left: vw-180},500);
+			$('#b44').animate({top:240, left: vw-80},500);
+			$('#b55').animate({top:240, left: vw+80},500);
+			$('#b66').animate({top:240, left: vw+180},500);
 			$('#b77').animate({top:240, left: vw+250},500);
 		});
 
